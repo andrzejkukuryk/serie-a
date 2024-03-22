9caac4b50ab299bfa4ab302f5f6d8b67f64eb71d
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2024-03-22 15:05:29 +0100

    add clubs

--- src/app/club/club.component.ts
+++ src/app/club/club.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+} from '@angular/core';
 import { Club } from '../../models/club';
 
 @Component({
@@ -8,4 +14,30 @@ import { Club } from '../../models/club';
 })
 export class ClubComponent {
   @Input() club?: Club;
+  public expanded = false;
+
+  @ViewChild('clubContainer') clubContainer!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  setPositionAbsolute() {
+    const container = this.clubContainer.nativeElement;
+
+    const rect = container.getBoundingClientRect();
+    const left = rect.left;
+    const top = rect.top;
+    // debugger;
+    console.log(rect.left, top);
+
+    this.renderer.setStyle(container, 'position', 'absolute');
+    this.renderer.setStyle(container, 'left', `${left}px`);
+    this.renderer.setStyle(container, 'top', `${top}px`);
+
+    console.log(rect);
+  }
+
+  handleClick(): void {
+    this.setPositionAbsolute();
+    this.expanded = true;
+  }
 }
