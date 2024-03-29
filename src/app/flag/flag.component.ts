a4f2736aebdd5783e31372414218b279d3702c40
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2024-03-29 14:56:26 +0100

    Auto-generated commit

--- src/app/flag/flag.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-flag',
-  templateUrl: './flag.component.html',
-  styleUrl: './flag.component.scss',
-})
-export class FlagComponent {}
